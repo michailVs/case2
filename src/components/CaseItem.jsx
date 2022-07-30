aa71030aa2c6c595c3e49e8d6fa8a76ba0e424d1
mihatormoz <mcohotnik21@gmail.com>
2022-07-31 00:04:14 +0300

    1

--- src/components/CaseItem.jsx
+++ src/components/CaseItem.jsx
@@ -5,8 +5,8 @@ const CaseItem = ({csi, setOpen, saleItem}) => {
             <div key={item.id}>
                 <img src={item.src} alt={item.title} />
                 <h2>{item.title}</h2>
-                <button onClick={setOpen}>Оставть предмет</button>
-                <button onClick={() => saleItem(item)}>Продать предмет за {item.price}</button>
+                <button onClick={setOpen}>Оставить предмет</button>
+                <button onClick={() => saleItem(item)}>Продать предмет за {item.price} руб.</button>
             </div>
             )
         }
